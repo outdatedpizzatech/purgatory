f07366369aff8cef5487a096d503240bf2be5120
u <u@MacBook-Pro.local>
2016-01-29 17:17:25 -0500

    fixed encounter resolution

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -16,33 +16,36 @@ public class BattleController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (inCombat) {
-			PartyMember activePartyMember = null;
-			foreach (PartyMember partyMember in PartyMember.members) {
-				if (partyMember.turnAvailable) {
-					activePartyMember = partyMember;
-					break;
-				}
-			}
-			if (activePartyMember == null && !GameController.frozen && EventQueue.instance.actionEvents.Count < 1) {
-				if (RoomController.instance.enemies.Count < 1) {
-					SpeechBubble.AddMessage ("all enemies eliminated", false);
-					BattleController.inCombat = false;
-					GameController.ExitEncounter ();
-				} else {
-					foreach (GameObject enemy in RoomController.instance.enemies) {
-						enemy.GetComponent<Corgi> ().DoAction ();
+			if (RoomController.instance.enemies.Count < 1) {
+				EventQueue.AddMessage ("all enemies eliminated");
+				BattleController.inCombat = false;
+			} else {
+				PartyMember activePartyMember = null;
+				foreach (PartyMember partyMember in PartyMember.members) {
+					if (partyMember.turnAvailable) {
+						activePartyMember = partyMember;
+						break;
 					}
 				}
-				foreach (PartyMember partyMember in PartyMember.members) {
-					partyMember.turnAvailable = true;
-				}
-			} else {
-				if (!combatMenuDisplayed && EventQueue.instance.actionEvents.Count < 1) {
-					combatMenuDisplayed = true;
-					print ("show me the menu");
-					EventQueue.AddShowCombatMenu (activePartyMember);
+				if (activePartyMember == null && EventQueue.instance.actionEvents.Count < 1) {
+					if (RoomController.instance.enemies.Count > 0) {
+						foreach (GameObject enemy in RoomController.instance.enemies) {
+							enemy.GetComponent<Corgi> ().DoAction ();
+						}
+					}
+					foreach (PartyMember partyMember in PartyMember.members) {
+						partyMember.turnAvailable = true;
+					}
+				} else {
+					if (!combatMenuDisplayed && EventQueue.instance.actionEvents.Count < 1) {
+						combatMenuDisplayed = true;
+						print ("show me the menu");
+						EventQueue.AddShowCombatMenu (activePartyMember);
+					}
 				}
 			}
+		} else {
+			if(EventQueue.instance.actionEvents.Count < 1) GameController.ExitEncounter ();
 		}
 	}
 
