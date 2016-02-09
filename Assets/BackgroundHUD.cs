49be97cc76e12658979dd6b2bd9eeb6a65c2253b
u <u@MacBook-Pro.local>
2016-02-09 00:09:49 -0500

    background layout

---
+++ Assets/BackgroundHUD.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundHUD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.Find ("EnemyField").gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
